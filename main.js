74981337f085c8f5be31cd762d7b47f52087f8ca
ProgrammerOwais <muhammadowaiskhan616@gmail.com>
2022-07-27 12:28:58 +0500

    new

---
+++ main.js
@@ -0,0 +1,12 @@
+let mainInput = document.querySelector(".form-input");
+
+function rangeInput() {
+  if (mainInput.value.length > 9) {
+    mainInput.value += "";
+  }
+}
+let userData {
+    phoneNumber:"29r420",
+    pinCode :"24345",
+    authCode: "335";
+};
