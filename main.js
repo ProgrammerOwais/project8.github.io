442d22e46399f6efe516eeba8ff5a3a214e56f9f
ProgrammerOwais <muhammadowaiskhan616@gmail.com>
2022-07-27 12:30:45 +0500

    new changes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let mainInput = document.querySelector(".form-input");
+// let mainInput = document.querySelector(".form-input");
 
-function rangeInput() {
-  if (mainInput.value.length > 9) {
-    mainInput.value += "";
-  }
-}
-let userData {
-    phoneNumber:"29r420",
-    pinCode :"24345",
-    authCode: "335";
-};
+// function rangeInput() {
+//   if (mainInput.value.length > 9) {
+//     mainInput.value += "";
+//   }
+// }
+// let userData {
+//     phoneNumber:"29r420",
+//     pinCode :"24345",
+//     authCode: "335";
+// };
