c0037223d949c8cb5b810d7563211366c4e55b17
ProgrammerOwais <muhammadowaiskhan616@gmail.com>
2022-07-27 22:13:06 +0500

    new changes

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 let mainInput = document.querySelector(".form-input");
 let circle1 = document.querySelectorAll(".circle");
+let container1 = document.querySelector(".container1");
+let container2 = document.querySelector(".container2");
+let container3 = document.querySelector(".container3");
+let container4 = document.querySelector(".container4");
+let submitBtn = document.querySelector(".submit-btn");
+
 // let circle2 = document.querySelector(".circle2");
 // let circle3 = document.querySelector(".circle3");
 // let circle4 = document.querySelector(".circle4");
 let numbers = document.querySelectorAll(".number-btn");
-
+let userData = [];
 let next = document.querySelector(".next");
-function inputRange() {
-  if (
-    mainInput.value.length == 3 ||
-    mainInput.value.length == 8 ||
-    mainInput.value.length == 10
-  ) {
-    mainInput.value += "  ";
-  }
-}
+// function inputRange() {
+//   if (
+//     mainInput.value.length == 3 ||
+//     mainInput.value.length == 8 ||
+//     mainInput.value.length == 10
+//   ) {
+//     mainInput.value += "  ";
+//   }
+// }
 let phoneNumber = "";
 next.addEventListener("click", () => {
-  phoneNumber += mainInput.value;
+  if (mainInput.value.length > 8) {
+    phoneNumber += mainInput.value;
+    userData.push("+63 " + phoneNumber);
+    container2.style.display = "block";
+    container1.style.display = "none";
+
+    let mobileNumber2 = document.querySelector(".number");
+    mobileNumber2.innerText = userData[0];
+  } else {
+    alert("Plz First register your phone number correctly");
+  }
 });
 let mpinNumber = "";
 for (let i = 0; i < numbers.length; i++) {
@@ -34,12 +50,44 @@ for (let i = 0; i < numbers.length; i++) {
     } else if (circle1[3].style.backgroundColor != "white") {
       circle1[3].style.backgroundColor = "white";
       mpinNumber += numbers[i].innerText;
+      userData.push(mpinNumber);
+
+      //   console.log(userData);
+      let mobileNumber = document.querySelector(".page3-para2");
+      mobileNumber.innerText = userData[0];
+      container2.style.display = "none";
+      container3.style.display = "block";
     }
   });
 }
-setInterval(() => {
-  if (mpinNumber.length === 4) {
-  }
-}, 500);
 
-console.log(mpinNumber);
+function focusChange1() {
+  document.getElementById("code2").focus();
+}
+function focusChange2() {
+  document.getElementById("code3").focus();
+}
+function focusChange3() {
+  document.getElementById("code4").focus();
+}
+function focusChange4() {
+  document.getElementById("code5").focus();
+}
+function focusChange5() {
+  document.getElementById("code6").focus();
+}
+let authCode = "";
+submitBtn.addEventListener("click", () => {
+  let codes = document.querySelectorAll(".auth-code");
+  if (codes[5].value != "") {
+    for (let i = 0; i < codes.length; i++) {
+      authCode += codes[i].value;
+    }
+    userData.push(authCode);
+    console.log(userData);
+    container3.style.display = "none";
+    container4.style.display = "block";
+  } else {
+    alert("Plz first fill the authentication code boxes");
+  }
+});
